0168c6bce0448ea08b0316b4166730355021bbaf
Nilajit05 <141957524+Nilajit05@users.noreply.github.com>
2023-11-02 10:34:56 +0530

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const Router=express.Router()
 
-// this is the entry point of all the api/v1/questions named url's
+// Main entry point 
 Router.use('/question',require('./questions'));
 Router.use('/options',require('./options'))
 
